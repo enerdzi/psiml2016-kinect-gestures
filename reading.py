ce56f2bf6d09195129fefcd321318fc4bbd2755b
Boris Majić <boris.majic@gmail.com>
2016-07-20 13:41:47 +0200

    Added label and skeleton import from Samples folder

--- reading.py
+++ reading.py
@@ -1,9 +1,12 @@
 import numpy as np
 
-def get_data(num) :
+import glob
+import os
 
-    labels = np.loadtxt('Labels' + num + '.txt')
-    skeletons = np.loadtxt('Skeleton' + num + '.txt')
+def get_data(num, sampleIndex) :
+
+    labels = np.loadtxt('Labels' + str(sampleIndex) + str(num) + '.txt')
+    skeletons = np.loadtxt('Skeleton' + str(sampleIndex) + str(num) + '.txt')
     frames = np.zeros((len(skeletons) / 20, 20, 9))
 
     for i in range(len(skeletons) / 20):
@@ -12,4 +15,28 @@ def get_data(num) :
 
     return labels, frames
 
-lb, fr = get_data('00151')
+#lb, fr = get_data(1,151)
+
+#path = r'C:\Users\Boris\Documents\Astronomija i Petnica uopšte\PSIML\Projekat\Fixed Input'
+os.chdir("C:\\Users\\Boris\\Documents\\PSIML\\Projekat\\Fixed Input")
+
+
+labels = np.empty([0,1], dtype=np.str)
+for file in glob.glob("Labels*.txt"):
+    labels = np.append(labels, file)
+    
+skeletons = np.epty([0,1], dtype = np.str)
+for file in glob.glob("Skeletons*.txt"):
+    skeletons = np.append(skeletons, file)
+    
+
+
+#TODO: Obavezno permutirati redosled Samplova, i to na isti način i za skeletone i za labele
+#TODO: Najbolje prvo generisati permuzaciju niza dužine ukupnog broja Samplova, pa ga 
+#      koristiti kao range za prolaženje kroz neki for ciklus    
+#EDIT: Ovo je zapravo ova linija ispod!
+
+pemutationArray = np.random.permutation(np.asarray(range(1,len(skeletons))))
+ 
+
+
